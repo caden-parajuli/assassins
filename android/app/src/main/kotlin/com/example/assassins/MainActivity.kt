ab64a254cb3b111dfe6e6d8d3187c2cef2e822f9
Caden Parajuli <caden.parajuli@member.fsf.org>
2024-08-25 23:15:45 -0400

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/assassins/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.assassins
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      with pkgs;
+      {
+        devShell = mkShell rec {
+          nativeBuildInputs = [ ];
+          buildInputs = [
+            clang
+            cmake
+            curl
+            git
+            ninja
+            pkg-config
+
+            gtk3
+            xz
+            libcxx
+            pcre2
+            mount
+
+            flutter
+          ];
+          LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
+        };
+      }
+    );
+}
